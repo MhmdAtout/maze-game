a5cdabb086e5bd7b85d7a4f3039f5b4c044bbf2e
MhmdAtout <mohamad.atout0@icloud.com>
2023-02-23 22:43:42 +0200

    accessing DOM from html

---
+++ game.js
@@ -0,0 +1,10 @@
+window.onload = function{
+
+    var status = document.getElementById('status');
+    var map = document.getElementById('game');
+    var score = document.getElementById('score');
+    var start = document.getElementById('start');
+    var walls = document.querySelectorAll('.boundary');
+    var end = document.getElementById('end');
+
+}
